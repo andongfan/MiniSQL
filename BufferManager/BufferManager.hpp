0be050372e5ea3467c9ec7d66f874ae464fcf390
zkx06111 <zkx06111@hotmail.com>
2020-05-31 10:22:41 +0800

    add buffermanager

---
+++ BufferManager/BufferManager.hpp
@@ -0,0 +1,28 @@
+#ifndef BUFFER_MANAGER_HEADER
+#define BUFFER_MANAGER_HEADER
+
+#include "Page.hpp"
+#include <string>
+
+class BufferManager
+{
+private:
+    Page *pages;
+
+    int getEmptyPageId();
+
+public:
+    BufferManager();
+    ~BufferManager();
+    // find an empty page
+    // if no such page exists
+    // throw one in buffer out
+    int getPageId(const std::string &filename, int x);
+    char *getPageAddress(int pageId);
+    void pinPage(int pageId);
+    void dirtPage(int pageId);
+    void unpinPage(int pageId);
+    void undirtPage(int pageId);
+};
+
+#endif
---
+++ BufferManager/Page.hpp
@@ -0,0 +1,35 @@
+#ifndef PAGE_HEADER
+#define PAGE_HEADER
+
+#include <string>
+
+const int BLOCK_SIZE = 4096;
+
+class Page // use the term "page" when it's in memory
+{
+    char data[BLOCK_SIZE];
+    bool isPinned; // pinned pages won't be thrown out
+    int isDirty;   // dirty pages are being written and cannot be thrown out
+    // difference between pinned and dirty pages:
+    // a page is pinned intentionally to increase efficiency
+    // a page is set to dirty when DBMS tries to modfiy it
+    time_t lastModifiedTime; // the latest timestamp during which the page is modified
+    std::string filename;
+    int offset;
+
+public:
+    Page();
+    void setPin(const bool &);
+    void setDirty(const int &);
+    bool getPin();
+    int getDirty();
+    char *getMemoryAddress();
+    clock_t getLastModifiedTime() const;
+    std::string getFilename();
+    int getOffset();
+    bool loadFile(const std::string &, int x);
+    void storeFile();
+    // returns whether the loading is successful
+};
+
+#endif
